680dcd74dd7d3784f39c8e2cf980a621edb62fe0
cbyun11 <141765103+cbyun11@users.noreply.github.com>
2023-08-09 00:18:25 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -25,7 +25,7 @@ custom_css = f"""
     <style>
     /* General Styling */
     .reportview-container {
-        background: #262C46;
+        background-color: #262C46;
     }
     /* Header Styling */
     h1, h2, h3, h4, h5, h6 {{
